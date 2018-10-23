0fe75dfd382f4d5ef56dee706f68e0441e236b47
Kaioh95 <42385515+Kaioh95@users.noreply.github.com>
2018-10-23 19:00:22 -0300

    Add files via upload

--- Projeto/CriaTabela.c
+++ Projeto/CriaTabela.c
@@ -13,29 +13,28 @@ void criar(void){
 		for(j=0; j<coluna; j++){
 			setbuf(stdin, NULL);
 			printf("Linha %d e coluna %d\n", i+1, j+1);
-			setbuf(stdin, NULL);
 			printf("Digite o tipo de dado: ");
 			scanf("%s", tipo);
 			
 			//Tratamento para cada tipo de dado
 			if(strcmp(tipo, "int") == 0){
 				setbuf(stdin, NULL);
-				printf("Digite: ");
+				printf("Digite i: ");
 				scanf("%d", &mat[i][j].numi);
 			}
 			else if(strcmp(tipo, "float") == 0){
 				setbuf(stdin, NULL);
-				printf("Digite: ");
+				printf("Digite f: ");
 				scanf("%f", &mat[i][j].numf);
 			}
 			else if(strcmp(tipo, "char") == 0){
 				setbuf(stdin, NULL);
-				printf("Digite: ");
+				printf("Digite c: ");
 				scanf("%c", &mat[i][j].letra);
 			}
 			else if(strcmp(tipo, "string") == 0){
 				setbuf(stdin, NULL);
-				printf("Digite: ");
+				printf("Digite s: ");
 				scanf("%s", &mat[i][j].string);
 			}
 		}
@@ -43,7 +42,12 @@ void criar(void){
 		
 	}
 
-
-
+	FILE *arquivo = fopen("teste.txt", "w");
+	for(i=0; i<linha; i++){
+		for(j=0; j<coluna; j++){
+			fprintf(arquivo, "%d\n", mat[i][j].numi);
+		}
+	}
+	fclose(arquivo);
 
 }
